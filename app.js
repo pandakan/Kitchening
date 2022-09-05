a785502eb5cfe24a850962c0a5dc03aa99135d29
Hernan Gryszkiewicz <rokoposter@yahoo.com.ar>
2022-09-05 11:17:39 -0300

    trabajo digital

---
+++ app.js
@@ -0,0 +1,17 @@
+const express = require("express");
+const app = express();
+const path = require("path");
+PORT = 3000;
+
+app.use(express.static(path.join(__dirname,'public')))
+
+app.get('/',(req, res) => {
+    res.sendFile(path.join(__dirname,'/views/index.html'))
+})
+
+app.listen(PORT, () => console.log('Servidor levantado en el puerto ' + PORT))
+
+
+
+
+
